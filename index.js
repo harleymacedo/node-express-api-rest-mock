897b47943aa69e698155859bae2e8ece708d92a2
harleymacedo <harley.ip@gmail.com>
2022-12-08 16:11:30 -0300

    Mudança para api de professores

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const alunoRouter = require('./routes/alunoRouter');
+const professorRouter = require('./routes/professorRouter');
 
 app.use(express.json());
-app.use(alunoRouter);
+app.use(professorRouter);
 
 app.listen(process.env.PORT || 3000);
---
+++ routes/professorRouter.js
@@ -0,0 +1,32 @@
+const professorRouter = require('express').Router();
+const professores = [
+    {nome: 'Marcos', area: 'Banco de dados'},
+    {nome: 'Ana', area: 'UX Design'},
+    {nome: 'Hugo', area: 'Programação'},
+    {nome: 'Diego', area: 'Inteligência artificial'},
+    {nome: 'Maria', area: 'Programação'},
+    {nome: 'Hugo', area: 'Engenharia de software'},
+    {nome: 'Danilo', area: 'Programação web e mobile'},
+    {nome: 'Yolanda', idade: 'Engenharia de software'},
+];
+
+professorRouter.get('/professor/todos', (req, res) => {
+    try {
+        res.json(professores);
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Não foi possivel recuperar os dados.'});
+    }
+});
+
+professorRouter.get('/professor/:nome', (req, res) => {
+    try {
+        const professorEncontrado = professores.find( (item) => {
+            return item.nome === req.params.nome;
+        });
+        res.json(professorEncontrado);
+    } catch (error) {
+        res.json({erro: true, mensagem: 'Não foi possivel recuperar os dados.'});
+    }
+});
+
+module.exports = professorRouter;
